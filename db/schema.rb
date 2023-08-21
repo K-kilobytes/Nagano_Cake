69fde7279d72830642cceacb9da60705242e4db0
EC2 Default User <ec2-user@ip-172-31-45-180.ap-northeast-1.compute.internal>
2023-08-21 04:54:54 +0000

    modify0821

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_20_082813) do
+ActiveRecord::Schema.define(version: 2023_08_20_082756) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
