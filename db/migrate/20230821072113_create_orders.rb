eefd54de4e19f6f0e47fbf07851b3f35865f90a8
EC2 Default User <ec2-user@ip-172-31-21-193.ap-northeast-1.compute.internal>
2023-08-21 14:28:01 +0000

    [Modify]table_spellmiss

--- db/migrate/20230821072113_create_orders.rb
+++ db/migrate/20230821072113_create_orders.rb
@@ -4,11 +4,11 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.integer :customer_id,     null: false, default: ""
       t.integer :postage,         null: false, default: ""
       t.integer :total_payment,   null: false, default: ""
-      t.string :address,          null: false, default: ""
-      t.string :post_code,        null: false, default: ""
-      t.string :name,             null: false, default: ""
+      t.string  :address,         null: false, default: ""
+      t.string  :post_code,       null: false, default: ""
+      t.string  :name,            null: false, default: ""
       t.integer :peyment_method,  null: false, default: "1"
-      t.intger :status,           null: false, default: "1"
+      t.integer :status,          null: false, default: "1"
 
       t.timestamps
     end
