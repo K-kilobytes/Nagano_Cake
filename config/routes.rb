e8bcd963f978cb567efa191ba62fa33da6f2f2c4
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-08-22 20:52:55 +0000

    make orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions',
-    homes: 'public/homes/homes'
   }
+  #namespace :customers do
+    get "/" => "public/homes#top"
+    get "/about" => "public/homes#about"
+    resources :orders, only: [:new, :index, :show, :complete]
+  #end
 
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
- 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
