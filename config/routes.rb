17a5339fada8b72579b0f656a0c898c06051eb6d
kikikip <135789408+kikikip@users.noreply.github.com>
2023-08-22 04:50:50 +0000

    [routes]items

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope module: :customer do
+   resources :items, only: %i[index show]
+  end
 end
