09410981499c5fb3a7698793f796213bfd5d642c
kikikip <135789408+kikikip@users.noreply.github.com>
2023-08-23 08:14:44 +0000

    [route]view display

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  scope module: :customer do
+  scope module: :public do
    resources :items, only: %i[index show]
   end
 end
