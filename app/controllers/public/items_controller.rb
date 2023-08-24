8402c76b4a2c00e8d7799e8841a8788dd8769c85
kikikip <135789408+kikikip@users.noreply.github.com>
2023-08-24 09:38:09 +0000

    [item]in the middle

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @items = Item.all
   end
   def show
+    @item = Item.find(params[:id])
+    @user = User.find(@Item.user_id)
+    @Itemn = Item.new
   end
 end
